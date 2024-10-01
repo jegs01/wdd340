d07c44ac8519dca860733f4f3658542e4f2b3dd8
Jegs01 <bodcafe@gmail.com>
2024-10-01 02:43:28 +0100

    Added more routes

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const staticRoutes = require("./routes/static");
+const inventoryRoute = require("./routes/inventoryRoute");
+const baseController = require("./controllers/baseController");
+const utilities = require("./utilities/");
 
 /* ***********************
  * View Engine and Templates
@@ -31,9 +34,30 @@ app.use(express.static('public'));
 app.use(staticRoutes);
 
 // Index Route
-app.get('/', (req, res) => {
-  res.render('index', {title: 'Home'});
-});
+app.get('/', utilities.handleErrors(baseController.buildHome));
+app.use("/inv", utilities.handleErrors(inventoryRoute));
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
